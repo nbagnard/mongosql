3f3f420a711fdf2d71f0875cfe0e66d3f0ae7c06
Ryan Chipman <ryan@ryanchipman.com>
2020-08-14 14:55:12 -0400

    have from_extern_string and to_extern_string helpers

--- mongosql-c/src/lib.rs
+++ mongosql-c/src/lib.rs
@@ -2,18 +2,22 @@ use std::{ffi::{CString, CStr}, os::raw};
 
 #[no_mangle]
 pub extern "C" fn version() -> *const raw::c_char {
-	extern_string("0.1.0-pre-test")
+	to_extern_string("0.1.0-pre-test")
 }
 
 #[no_mangle]
 pub extern "C" fn translate(sql: *const libc::c_char) -> *const raw::c_char {
-	let sql = unsafe { CStr::from_ptr(sql).to_bytes() };
-	let sql = String::from_utf8(sql.to_vec()).unwrap();
+	let sql = from_extern_string(sql);
 	let pipeline = mongosql::translate_sql(&sql);
-	extern_string(&pipeline)
+	to_extern_string(&pipeline)
 }
 
-fn extern_string(s: &str) -> *const raw::c_char {
+fn from_extern_string(s: *const libc::c_char) -> String {
+	let s = unsafe { CStr::from_ptr(s).to_bytes() };
+	String::from_utf8(s.to_vec()).unwrap()
+}
+
+fn to_extern_string(s: &str) -> *const raw::c_char {
     // build a new nul-terminated string
     let c_str = CString::new(s).unwrap();
 
