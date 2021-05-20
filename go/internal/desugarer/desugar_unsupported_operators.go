eb74f384047c2cade2da66975d8aa87e8afd4288
Matthew Chiaravalloti <matthew.chiaravalloti@mongodb.com>
2021-05-20 10:37:38 -0400

    SQL-280: Implement $nullIf desugaring (#116)

--- go/internal/desugarer/desugar_unsupported_operators.go
+++ go/internal/desugarer/desugar_unsupported_operators.go
@@ -10,6 +10,7 @@ var functionDesugarers = map[string]functionDesugarer{
 	"$sqlBetween": desugarSQLBetween,
 	"$sqlSlice":   desugarSQLSlice,
 	"$sqlDivide":  desugarSQLDivide,
+	"$nullIf":     desugarNullIf,
 }
 
 // desugarUnsupportedOperators desugars any new operators ($sqlBetween,
@@ -77,5 +78,31 @@ func desugarSQLDivide(f *ast.Function) ast.Expr {
 		ast.NewBinary(ast.Equals, divisor, zeroLiteral),
 		onError,
 		ast.NewBinary(ast.Divide, dividend, divisor),
+	)
+}
+
+// desugarNullIf desugars { $nullIf: [<expr1>, <expr2>] } into existing MQL
+// operators.
+// Note that $nullIf returns null if <expr1> = <expr2> is true, and otherwise
+// returns <expr1>. Like all MQL operators, $nullIf should never return missing
+// so the desugared version wraps <expr1> in a $ifNull to promote missing to
+// null if necessary.
+func desugarNullIf(f *ast.Function) ast.Expr {
+	args := f.Arg.(*ast.Array)
+
+	expr1VarName := "expr1"
+	expr1VarRef := ast.NewVariableRef(expr1VarName)
+
+	return ast.NewLet(
+		[]*ast.LetVariable{
+			ast.NewLetVariable(expr1VarName,
+				wrapInOp("$ifNull", args.Elements[0], nullLiteral),
+			),
+		},
+		ast.NewConditional(
+			ast.NewBinary(ast.Equals, expr1VarRef, args.Elements[1]),
+			nullLiteral,
+			expr1VarRef,
+		),
 	)
 }
