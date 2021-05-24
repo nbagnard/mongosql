76b55ca7beaa9018d9576b9454fc7a6b65c10430
huan-Mongo <53796565+huan-Mongo@users.noreply.github.com>
2021-05-24 14:51:03 -0400

    SQL-328: Implement sqlAnd, sqlOr desugaring function (#125)

--- go/internal/desugarer/desugar_sql_null_semantics.go
+++ go/internal/desugarer/desugar_sql_null_semantics.go
@@ -30,7 +30,11 @@ func desugarSQLNullSemantics(pipeline *ast.Pipeline, _ uint64) *ast.Pipeline {
 	out, _ := ast.Visit(pipeline, func(v ast.Visitor, n ast.Node) ast.Node {
 		switch tn := n.(type) {
 		case *ast.Function:
-			if mqlOp, ok := nullSemanticFuncMapping[tn.Name]; ok {
+			if tn.Name == "$sqlAnd" {
+				n = desugarSqlAnd(tn)
+			} else if tn.Name == "$sqlOr" {
+				n = desugarSqlOr(tn)
+			} else if mqlOp, ok := nullSemanticFuncMapping[tn.Name]; ok {
 				n = desugarSqlOp(tn, mqlOp)
 			}
 		}
@@ -97,6 +101,180 @@ func desugarSqlOp(f *ast.Function, mqlOp string) ast.Expr {
 			return ast.NewLet(assignments, evaluation)
 		}
 	default:
-		panic("Unsupported argument when desurgaring null semantics")
+		panic("Unsupported argument when desugaring null semantics")
 	}
+}
+
+// desugarSQLAnd desugars a $sqlAnd expression to a nested $cond expression. In the first level of the $cond,
+// it checks if any of the input variable is False, if so, return False literal directly. Otherwise in the second level
+// it checks if any of the input variable is null, if so, return Null directly, otherwise return True literal.
+// {$'sqlAnd': [<expr1>, <expr2>]}
+//
+// will be desugared into
+//
+//  "$let": {
+//    "vars": {
+//      "desugared_sqlAnd_input0": "$<expr1>",
+//      "desugared_sqlAnd_input1": "$<expr2>"
+//    },
+//    "in": {
+//      "$cond": {
+//        "if": {
+//          "$or": [
+//            // check if any of the vars is False
+//          ]
+//        },
+//        "then": {
+//          "$literal": false
+//        },
+//        "else": {
+//          "$cond": {
+//            "if": {
+//                // check if any of the vars is Null
+//            },
+//            "then": {
+//              "$literal": null
+//            },
+//            "else": {
+//              "$literal": true
+//            }
+//          }
+//        }
+//      }
+//    }
+//  }
+func desugarSqlAnd(f *ast.Function) ast.Expr {
+	args, ok := f.Arg.(*ast.Array)
+	if !ok {
+		panic("Unsupported argument when desugaring $sqlAnd")
+	}
+	numChildren := len(args.Elements)
+
+	assignments := make([]*ast.LetVariable, 0, numChildren)
+	newArgs := make([]ast.Expr, 0, numChildren)
+	nullCheckArgs := make([]ast.Expr, 0, numChildren)
+	falseCheckArgs := make([]ast.Expr, 0, numChildren)
+
+	for i, arg := range args.Elements {
+		constant, isConstant := arg.(*ast.Constant)
+		if isConstant && constant.Value.Type != bsontype.Null {
+			// The arg is a non-null literal, we will not create $let binding or check null value for it
+			newArgs = append(newArgs, arg)
+			falseCheckArgs = append(falseCheckArgs, arg)
+		} else {
+			binding := fmt.Sprintf("desugared_sqlAnd_input%d", i)
+			ref := ast.NewVariableRef(binding)
+
+			assignments = append(assignments, ast.NewLetVariable(binding, arg))
+			newArgs = append(newArgs, ref)
+			falseCheckArgs = append(falseCheckArgs, ref)
+			nullCheckArgs = append(nullCheckArgs, ref)
+		}
+	}
+
+	nullCheckedCond := wrapInNullCheckedCond(
+		// if any operand is null, return null.
+		nullLiteral,
+		// else return true
+		trueLiteral,
+		nullCheckArgs...,
+	)
+
+	// First check if any of the input param is evaluated to False, if so, evaluate the whole expression to False.
+	var condition ast.Expr
+	conds := make([]ast.Expr, len(falseCheckArgs))
+	for i, cond := range falseCheckArgs {
+		conds[i] = ast.NewBinary("$eq", cond, falseLiteral)
+	}
+	condition = wrapInOp("$or", conds...)
+
+	evaluation := ast.NewConditional(condition, falseLiteral, nullCheckedCond)
+
+	return ast.NewLet(assignments, evaluation)
+}
+
+// desugarSqlOr desugars a $sqlOr expression to a nested $cond expression. In the first level of the $cond,
+// it checks if any of the input variable is True, if so, return True literal directly. Otherwise in the second level
+// it checks if any of the input variable is null, if so, return Null directly, otherwise return False literal.
+// {$'sqlOr': [<expr1>, <expr2>]}
+//
+// will be desugared into
+//
+//  "$let": {
+//    "vars": {
+//      "desugared_sqlOr_input0": "$<expr1>",
+//      "desugared_sqlOr_input1": "$<expr2>"
+//    },
+//    "in": {
+//      "$cond": {
+//        "if": {
+//            // check if any of the vars is True
+//        },
+//        "then": {
+//          "$literal": true
+//        },
+//        "else": {
+//          "$cond": {
+//            "if": {
+//              // check if any of the vars is Null
+//            },
+//            "then": {
+//              "$literal": null
+//            },
+//            "else": {
+//              "$literal": false
+//            },
+//          }
+//        }
+//      }
+//    }
+//  }
+func desugarSqlOr(f *ast.Function) ast.Expr {
+	args, ok := f.Arg.(*ast.Array)
+	if !ok {
+		panic("Unsupported argument when desugaring $sqlOr")
+	}
+	numChildren := len(args.Elements)
+
+	assignments := make([]*ast.LetVariable, 0, numChildren)
+	newArgs := make([]ast.Expr, 0, numChildren)
+	nullCheckArgs := make([]ast.Expr, 0, numChildren)
+	trueCheckArgs := make([]ast.Expr, 0, numChildren)
+
+	for i, arg := range args.Elements {
+		constant, isConstant := arg.(*ast.Constant)
+		if isConstant && constant.Value.Type != bsontype.Null {
+			// The arg is a non-null literal, we will not create $let binding or check null value for it
+			newArgs = append(newArgs, arg)
+			trueCheckArgs = append(trueCheckArgs, arg)
+		} else {
+			binding := fmt.Sprintf("desugared_sqlOr_input%d", i)
+			ref := ast.NewVariableRef(binding)
+
+			assignments = append(assignments, ast.NewLetVariable(binding, arg))
+			newArgs = append(newArgs, ref)
+			trueCheckArgs = append(trueCheckArgs, ref)
+			nullCheckArgs = append(nullCheckArgs, ref)
+		}
+	}
+
+	nullCheckedCond := wrapInNullCheckedCond(
+		// if any operand is null, return null.
+		nullLiteral,
+		// else return false
+		falseLiteral,
+		nullCheckArgs...,
+	)
+
+	// First check if any of the input param is evaluated to True, if so, evaluate the whole expression to True.
+	var condition ast.Expr
+	conds := make([]ast.Expr, len(trueCheckArgs))
+	for i, cond := range trueCheckArgs {
+		conds[i] = ast.NewBinary("$eq", cond, trueLiteral)
+	}
+	condition = wrapInOp("$or", conds...)
+
+	evaluation := ast.NewConditional(condition, trueLiteral, nullCheckedCond)
+
+	return ast.NewLet(assignments, evaluation)
 }
