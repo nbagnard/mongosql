32b15dfabb82e43ae89ade8842f3064baac59008
Oliver Bucaojit <oliver.bucaojit@mongodb.com>
2024-10-16 16:46:07 -0700

    SQL-2376: Rename schema-builder-tool to mongodb-schema-manager (#837)
    * SQL-2376: Rename schema-builder-tool to mongodb-schema-manager
    
    * SQL-2376: apply rustfmt
    
    * SQL-2376: Clippy fix

--- evergreen/resources/sign_macos_mongodb_schema_manager_zip.sh
+++ evergreen/resources/sign_macos_mongodb_schema_manager_zip.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Uses macnotary to sign and notarize the mac executable `schema-builder-tool-macos`.
+# Uses macnotary to sign and notarize the mac executable `mongodb-schema-manager-macos`.
 # The key id and secret were set as MACOS_NOTARY_KEY and MACOS_NOTARY_SECRET
 # env vars from the expansions. The macnotary client will look for these env
 # vars so we don't need to pass the credentials as CLI options.
@@ -10,8 +10,8 @@ set -o errexit
 set -o verbose
 
 # Turn the executable into a zip
-zip -r schema-builder-tool.zip schema-builder-tool-macos
-rm schema-builder-tool-macos
+zip -r mongodb-schema-manager.zip mongodb-schema-manager-macos
+rm mongodb-schema-manager-macos
 
 curl -LO https://macos-notary-1628249594.s3.amazonaws.com/releases/client/v3.3.3/darwin_amd64.zip
 unzip darwin_amd64.zip
@@ -20,15 +20,15 @@ chmod 0755 ./darwin_amd64/macnotary
 ./darwin_amd64/macnotary -v
 
 ./darwin_amd64/macnotary \
-    --task-comment "Signing the Schema Builder Tool" \
-    --file "$PWD/schema-builder-tool.zip" \
+    --task-comment "Signing the MongoDB Schema Manager" \
+    --file "$PWD/mongodb-schema-manager.zip" \
     --mode notarizeAndSign \
     --url https://dev.macos-notary.build.10gen.cc/api \
     --bundleId com.mongodb.mongosql \
-    --out-path "$PWD/schema-builder-tool-signed.zip"
+    --out-path "$PWD/mongodb-schema-manager-signed.zip"
 
-unzip schema-builder-tool-signed.zip
-if [ ! -f "schema-builder-tool-macos" ]; then
-    echo "Error: schema-builder-tool-macos does not exist."
+unzip mongodb-schema-manager-signed.zip
+if [ ! -f "mongodb-schema-manager-macos" ]; then
+    echo "Error: mongodb-schema-manager-macos does not exist."
     exit 1
 fi
